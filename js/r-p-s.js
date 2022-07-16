693cc9345d33121971bc38ec17c14927934a7b7e
Danky <oregonjonathan@gmail.com>
2022-07-16 04:13:29 -0700

    add single game

--- js/r-p-s.js
+++ js/r-p-s.js
@@ -1,29 +1,43 @@
 //ask do you wanna playjanken
 //get users choice text input
-function getUsersChoic(choice){
+function getUsersChoice(choice){
     while (true){
     choice = window.prompt('Please enter Rock,Paper,scissors or Guu, Paa, Choki: ');
     lowerCase= choice.toLowerCase();
-    if (lowerCase === 'rock' || lowerCase === 'guu'){ choice = "Guu!!(Rock)";break;
-}else if (lowerCase ==='paper' || lowerCase ==='paa'){ choice = "Paa!(Paper)"; break;
-}else if (lowerCase === ' scissors' || lowerCase === 'choki' ){choice = "Choki!!(Scissors)"; break;
+    if (lowerCase === 'rock' || lowerCase === 'guu'){choice = "rock";break;
+}else if (lowerCase ==='paper' || lowerCase ==='paa'){choice = "paper"; break;
+}else if (lowerCase === 'scissors' || lowerCase === 'choki' ){choice = "scissors"; break;
 }
     }
 console.log("Users choice: " + choice);
+return choice;
 }
-getUsersChoic()
-
 //get computer's choice
 function getComputerChoice(choice){let randomNum= Math.floor(Math.random() * 3) + 1;
-if (randomNum===1){ choice = "Guu!!(Rock)";
-} else if (randomNum===2){ choice = "Paa!(Paper)";
-} else { choice = "Choki!!(Scissors)";}
+if (randomNum===1){ choice = "rock";
+} else if (randomNum===2){ choice = "paper";
+} else { choice = "scissors";}
 console.log("pc Choice: " + choice);
 return choice;}
 
-getComputerChoice()
 //start game. Saisho wa guu (Starting with stone)
 // show hands "Janken pon!"
 //show winner
+const usr = getUsersChoice()
+console.log(usr);
+const pc = getComputerChoice()
+console.log(pc);
+function game(){
+    //you lose
+if ( usr === 'rock' && pc === 'paper'){return "you lose! Paper(Paa) beats Rock(Guu)!";
+}else if (usr === 'paper' && pc === 'scissors'){return "you lose! Scissors(Choki) beats Paper(Paa)!";
+}else if (usr === 'scissors' && pc === 'rock'){return "you lose! Rock(Guu) beats Scissors(Choki)!";
+} else if  (usr === 'paper' && pc === 'rock'){return "you win! Paper(Paa) beats Rock(Guu)!";
+} else if  (usr === 'rock' && pc === 'scissors'){return "you win! Scissor(Choki) beats Paper(Paa)!";
+} else if  (usr === 'scissors' && pc === 'paper'){return "you win! Scissors(Choki) beats Paper(Paa)!";
+} else {return "It/s a draw!";
+}
+}
+console.log(game());
 //play again
 
